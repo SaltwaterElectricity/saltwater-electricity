801d6bae00b8c1509be2397792055e35d85f465e
Donquillo <junodonquillo@gmail.com>
2026-05-18 11:35:46 +0800

    AI fix error in router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
@@ -8,12 +9,14 @@ import { UIProvider } from "./context/UIContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <NotificationProvider>
-        <UIProvider>
-          <App />
-        </UIProvider>
-      </NotificationProvider>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <NotificationProvider>
+          <UIProvider>
+            <App />
+          </UIProvider>
+        </NotificationProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
